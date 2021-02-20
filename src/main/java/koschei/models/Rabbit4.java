2efbb6a9e73b2a844a76dc7697ed6a1a4e2d9a30
levgenix <mr.lobach@gmail.com>
2021-02-20 16:21:50 +0300

    Task completed

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+    private final Duck5 duck;
+
+    @Autowired
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
+    }
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
